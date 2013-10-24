365d513a6a8057ab70065202555a9c76c29352b0
Marcus Nilsen <post@marcusnilsen.no>
2013-10-25 01:19:25 +0200

    added a few variables and cleaned up code

--- showSong.py
+++ showSong.py
@@ -13,28 +13,33 @@
 #
 #	by @marcusnilsen, 24.10.2013
 #
-# Importing Apple Script binding
+
+# Import Apple Script binding
 from Foundation import *
 from ScriptingBridge import *
 
-# Process binding
-Processes = SBApplication.applicationWithBundleIdentifier_("com.apple.systemevents")
+# Set up the system process binding
+SystemApps = SBApplication.applicationWithBundleIdentifier_("com.apple.systemevents")
+
+# System bundle identifiers for VLC/iTunes
+VLCBundle = "org.videolan.vlc"
+iTunesBundle = "com.apple.iTunes"
 
 # Set default song playing to none
 playing = 0
 
-for item in Processes.processes():
+for app in SystemApps.processes():
 	# Listing processes to see if VLC/iTunes are running
 	# to prevent them from starting if they are not currently running
-	if item.bundleIdentifier() == "org.videolan.vlc":
-		VLC = SBApplication.applicationWithBundleIdentifier_("org.videolan.vlc")
+	if app.bundleIdentifier() == VLCBundle:
 		# VLC Code
+		VLC = SBApplication.applicationWithBundleIdentifier_(VLCBundle)
 		if VLC.playing():
 			print "/me is listning to '" + VLC.nameOfCurrentItem() + "' (VLC/iTerm2)"
 			playing = 1
-	elif item.bundleIdentifier() == "com.apple.iTunes":
+	elif app.bundleIdentifier() == iTunesBundle:
 		# iTunes Code
-		iTunes = SBApplication.applicationWithBundleIdentifier_("com.apple.iTunes")
+		iTunes = SBApplication.applicationWithBundleIdentifier_(iTunesBundle)
 		# INT representing state
 		if (iTunes.playerState() == 1800426320):
 			playing = 1
